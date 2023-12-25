156571a0f6eb2cb06c4f714fc2de130183d2875c
MSI\timot <timothyfuapple@gmail.com>
2023-12-25 20:16:50 +1030

    Initial project commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Experience from './components/Experience/Experience';
+import Contact from './components/Contact/Contact';
+
+import BgVideo from './components/BgVideo/BgVideo';
+import LoadingAnimation from './components/LoadingAnimation/LoadingAnimation';
+import SideNav from './components/SideNav/SideNav';
+import ReactJSProjects from './components/ReactJSProjects/ReactJSProjects';
+import JavaProjects from './components/JavaProjects/JavaProjects';
+import AIProjects from './components/AIProjects/AIProjects';
+import PythonProjects from './components/PythonProjects/PythonProjects';
+import VueJSProjects from './components/VueJSProjects/VueJSProjects';
+import CSharpProjects from './components/CSharpProjects/CSharpProjects';
+import CPlusPlusProjects from './components/CPlusPlusProjects/CPlusPlusProjects';
+import NodeJSProjects from './components/NodeJSProjects/NodeJSProjects';
 
 function App() {
   return (
